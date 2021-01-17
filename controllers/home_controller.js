3fb3557f31a710c81abcde0240158eb948ffa520
Utkarsh Nigam <nutkarsh706@gmail.com>
2021-01-17 20:18:00 +0530

    Added posts section to the website

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,29 @@
+const Post = require('../models/post')
+
 module.exports.home = function(req,res){
-    res.cookie('user_id',25);
-   return res.render('home',{
-       title: "Home"
-   });
+    //  res.cookie('user_id',25);
+    // Post.find({},function(err,posts){
+    //     if(err){
+    //         console.log('Error in fetching contacts from posts');
+    //         return;
+    //     }
+    //   //  console.log('Todo:',Todo);
+    // return res.render('home',{
+    //     title: "Codial | Home",
+    //     post_List: posts
+    // });
+    // });
+
+    //populate the user of each post
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title: "Codeial|Home",
+            posts:posts
+        }
+    );})
 } 
 
 module.exports.actionName = function(req,res){
     return res.end('<p>We will rise</p>')
 }
+
